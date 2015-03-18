827b67fc857a133c2d7ee0e82cbe824b2ceabd1b
Patrick Werner <pwerner2@gmail.com>
2015-03-18 14:46:44 -0400

    ember data problem. will be uprooting and retrying very soon

--- app/router.js
+++ app/router.js
@@ -1,11 +1,57 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
-  location: config.locationType
+App.Taskable = DS.Model.extend({
+
+    tasks: DS.hasMany('task', {async: true})
 });
 
-Router.map(function() {
+App.Task = DS.Model.extend({
+
+    subject: DS.attr('string'),
+
+    taskable: DS.belongsTo('taskable', {polymorphic: true})
 });
 
-export default Router;
+App.Company = App.Taskable.extend({
+
+    name: DS.attr('string'),
+
+    people: DS.hasMany('person', {async: true})
+});
+
+App.Person = App.Taskable.extend({
+
+    firstName: DS.attr('string'),
+
+    lastName: DS.attr('string'),
+
+    city : DS.attr('string'),
+
+    state: DS.attr('string'),
+
+    email: DS.attr('string'),
+
+    fullName: function() {
+
+        return this.get('firstName') + ' ' + this.get('lastName');
+
+    }.property('firstName', 'lastName'),
+
+    location: function() {
+
+        var location = this.get('city');
+
+        var state = this.get('state');
+
+            if (state && state.trim().length > 0) {
+
+                location += ', ' + state;
+            }
+
+        return location;
+
+        }.property('city', 'state'),
+
+        company: DS.belongsTo('company', {async: true})
+});
