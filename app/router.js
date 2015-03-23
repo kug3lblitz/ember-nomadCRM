b81c09756d5b9b79f6fc7085e7ad6133b62ecfa7
Patrick Werner <pwerner2@gmail.com>
2015-03-23 09:58:30 -0400

    added simpleauth, corrected root route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route("home");
+    this.route("/");
     this.resource("companies", function() {
         this.route("new");
     });
