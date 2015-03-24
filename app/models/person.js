08a9ac45923446ddba3f0648b4a3c6092f06e1d7
Patrick Werner <pwerner2@gmail.com>
2015-03-24 17:19:36 -0400

    eliminated redundant menu item

--- app/models/person.js
+++ app/models/person.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+    name: DS.attr('string'),
+    company: DS.attr('string'),
+    address: DS.attr('string'),
+    phone: DS.attr('string'),
+    website: DS.attr('string'),
+    industry: DS.attr('string'),
+    notes: DS.hasMany('note', {async: true})
 });
