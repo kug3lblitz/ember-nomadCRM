38cb095b098bf80821ebdc132c9d2dd717657c06
Patrick Werner <pwerner2@gmail.com>
2015-03-26 11:15:36 -0400

    pre route nesting commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
     this.route("/");
+    //this.resource('person', { path: ':person_name'}
     this.resource("people", function() {
         this.route("new");
     });
