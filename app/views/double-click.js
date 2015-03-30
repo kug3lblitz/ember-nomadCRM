6b48cfff0eaaebc552e057246feac47397c6619e
Patrick Werner <pwerner2@gmail.com>
2015-03-30 00:01:29 -0400

    getting error message on focusout, progress

--- app/views/double-click.js
+++ app/views/double-click.js
@@ -20,6 +20,7 @@ export default Ember.View.extend({
 
     textField: Ember.TextField.extend({
         focusOut: function() {
+          this.saveRecord();
           this.save();
         },
 
@@ -32,5 +33,31 @@ export default Ember.View.extend({
           }
           parentView.set('isEditing', false);
         }
-    })
+    }),
+
+      saveRecord: function(name, record) {
+        if(record.id) {
+          return ajax({
+            url: "https://api.parse.com/1/classes/Person/" + record.id,
+            type: "PUT",
+            data: JSON.stringify(record.toJSON())
+          }).then(function(response) {
+            record.updatedAt = response.updatedAt;
+            record.sessionToken = response.sessionToken;
+            return record;
+          });
+
+        } else {
+          return ajax({
+            url: "https://api.parse.com/1/classes/Person",
+            type: "POST",
+            data: JSON.stringify(record.toJSON())
+          }).then(function(response) {
+            record.id = response.objectId;
+            record.createdAt = response.createdAt;
+            record.sessionToken = response.sessionToken;
+            return record;
+          });
+        }
+      }
 });
