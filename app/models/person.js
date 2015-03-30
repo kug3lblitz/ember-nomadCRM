02dfa2620cf08a9b7e665741ea92559844850e7c
Patrick Werner <pwerner2@gmail.com>
2015-03-29 23:49:26 -0400

    moved login button

--- app/models/person.js
+++ app/models/person.js
@@ -2,21 +2,30 @@ import ajax from 'ic-ajax';
 import Model from 'ember-magic-man/model';
 
 export default Model.extend({
-  saveRecord: function(person) {
-    return ajax("https://api.parse.com/1/classes/Person/" + this.id, {
-      type: "PUT",
-      data: JSON.stringify({
-        favorites: {
-          __op: "AddRelation",
-          objects: [
-            {
-              __type: 'Pointer',
-              className: 'Person',
-              objectId: person.id
-            }
-          ]
-        }
-      })
-    });
+
+  saveRecord: function(name, record) {
+    if(record.id) {
+      return ajax({
+        url: "https://api.parse.com/1/classes/Person/" + record.id,
+        type: "PUT",
+        data: JSON.stringify(record.toJSON())
+      }).then(function(response) {
+        record.updatedAt = response.updatedAt;
+        record.sessionToken = response.sessionToken;
+        return record;
+      });
+
+    } else {
+      return ajax({
+        url: "https://api.parse.com/1/classes/Person",
+        type: "POST",
+        data: JSON.stringify(record.toJSON())
+      }).then(function(response) {
+        record.id = response.objectId;
+        record.createdAt = response.createdAt;
+        record.sessionToken = response.sessionToken;
+        return record;
+      });
+    }
   }
 });
