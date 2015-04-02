13288784626ce9f2a8f7651eca5a103eaa937443
Patrick Werner <pwerner2@gmail.com>
2015-04-02 02:42:33 -0400

    fixed broken PUT

--- app/controllers/people/person.js
+++ app/controllers/people/person.js
@@ -3,30 +3,25 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
       
-    createRecord: function(){
-        this.get('model').save().then(function(){
-            this.transitionToRoute('index');
-        }.bind(this));
-   },
+       createRecord: function(){
+             this.get('model').save().then(function(){
+                this.transitionToRoute('index');
+            }.bind(this));
+      },
        
-    editRecord: function() {
-        this.set('isEditing', true);
-   },
+        editRecord: function() {
+            this.set('isEditing', true);
+      }
+    },
 
-    acceptChanges: function() {
+    save: function() {
         this.set('isEditing', false);
-
-        if (Ember.isEmpty(this.get('model.title'))) {
-          this.send('removePerson');
-            } else {
-          this.get('model').save();
-        }
+        this.get('model').save();
     },
   
-    removeRecord: function() {
+    removePerson: function() {
         var person = this.get('model');
         person.destroy();
         person.save();
     }
- }
 });
