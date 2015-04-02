c9a06ebe426dd2ef26c550d830c08f2f76a68fbb
Patrick Werner <pwerner2@gmail.com>
2015-04-02 15:35:29 -0400

    one day I'll learn to commit from the proper path

--- backend/cloud/main.js
+++
@@ -1,9 +0,0 @@
-Parse.Cloud.define('search', function(request, response){
-  var query = new Parse.Query("Person");
-  query.contains("url", request.params.search);
-  query.find().then(function(results) {
-    response.success(results);
-  }, function(error) {
-    response.error(error);
-  });
-});
