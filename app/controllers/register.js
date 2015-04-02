e1b9f5ee43464d480dc2c9b28a704ef24b1e4b40
Patrick Werner <pwerner2@gmail.com>
2015-04-02 16:28:41 -0400

    go to records from register, progress on search, but not returning records

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -3,8 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         save: function(){
-            this.get('model').save().then(function(){
-                this.transitionToRoute('records');
+            this.get('model').save().then(function(user){
+                this.get('session').authenticate('authenticator:parse-email', user).then(function(){
+                    this.transitionToRoute('records');
+                }.bind(this));
             }.bind(this));
         },
     }
--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,9 +1,37 @@
 Parse.Cloud.define('search', function(request, response){
-  var query = new Parse.Query("Person");
-  query.contains("url", request.params.search);
+  var nameQuery = new Parse.Query("name");
+  nameQuery.matches("session.currentuser.person.model.name", new RegExp(request.params.search, 'i'));
+
+  var companyQuery = new Parse.Query("company");
+  companyQuery.matches("session.currentuser.person.model.company", new RegExp(request.params.search, 'i'));
+
+  var addressQuery = new Parse.Query("address");
+  addressQuery.matches("session.currentuser.person.model.address", new RegExp(request.params.search, 'i'));
+  
+  var phoneQuery = new Parse.Query("phone");
+  phoneQuery.matches("session.currentuser.person.model.phone", new RegExp(request.params.search, 'i'));
+  
+  var websiteQuery = new Parse.Query("website");
+  websiteQuery.matches("session.currentuser.person.model.website", new RegExp(request.params.search, 'i'));
+  
+  var industryQuery = new Parse.Query("industry");
+  industryQuery.matches("session.currentuser.person.model.industry", new RegExp(request.params.search, 'i'));
+
+  var query = Parse.Query.or(nameQuery, companyQuery, addressQuery, phoneQuery, websiteQuery, industryQuery);
   query.find().then(function(results) {
     response.success(results);
   }, function(error) {
     response.error(error);
   });
 });
+
+
+//Parse.Cloud.define('search', function(request, response){
+  //var query = new Parse.Query("Person");
+  //query.contains("url", request.params.search);
+  //query.find().then(function(results) {
+    //response.success(results);
+  //}, function(error) {
+    //response.error(error);
+  //});
+//});
